c1656e511b43c0e7a76fe9fa6768e97a1b4194ff
Sampo <pelto.sampo@gmail.com>
2018-09-03 02:35:55 +0300

    Basic structure and function

---
+++ weather.py
@@ -0,0 +1,52 @@
+import sys
+from configparser import ConfigParser
+import pyowm
+
+
+def main():
+
+    cfg = ConfigParser()
+    cfg.read('token.cfg')
+
+    owm = pyowm.OWM(cfg['PYOWN']['token'])
+
+    country = 'FI'
+    city = 'Tampere'
+
+    if len(sys.argv) == 2:
+        country = 'FI'
+        city = sys.argv[1]
+    elif len(sys.argv) == 3:
+        country = sys.argv[2]
+        city = sys.argv[1]
+
+    place = str(city + "," + country)
+    observation = owm.weather_at_place(place)
+    w = observation.get_weather()
+    condition = str(w).split('status=')[1][:-1].lower()
+
+    if condition == "clear":
+        text = "There is currently " + str(int(w.get_temperature('celsius')['temp'])) + \
+               "°C in " + city.capitalize() + " and it is " + condition + "."
+
+    elif condition == "clouds" or condition == "mist":
+        text = "There is currently " + str(int(w.get_temperature('celsius')['temp'])) + \
+               "°C in " + city.capitalize() + " and it is " + condition + "y."
+
+    elif condition == "snow":
+        text = "There is currently " + str(int(w.get_temperature('celsius')['temp'])) + \
+               "°C in " + city.capitalize() + " and it is " + condition + "ing."
+
+    elif condition == "fog":
+        text = "There is currently " + str(int(w.get_temperature('celsius')['temp'])) + \
+               "°C in " + city.capitalize() + " and it is " + condition + "gy."
+
+    else:
+        text = "There is currently " + str(int(w.get_temperature('celsius')['temp'])) + \
+               "°C in " + city.capitalize() + " and it " + condition + "s."
+
+    print(text)
+
+
+if __name__ == '__main__':
+    main()
